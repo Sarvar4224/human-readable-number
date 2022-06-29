7d43b36f07643b094e0f432a8e3a81c9a01a4be3
Sarvar4224 <sarvar.akh888@gmail.com>
2022-06-29 21:36:20 +0500

    feat:new solutions

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ module.exports = function toReadable (number) {
   let arr1 = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'],
       arr2 = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
       arr3 = ['ten', 'twenty', 'thirty','forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+      if(number==0)
+      return "zero"
       if (number<100 && number%10==0)
       return arr3[number/10-1];
       if (number<10)
@@ -29,6 +31,10 @@ module.exports = function toReadable (number) {
         else if(r==0)
         {
             return arr1[number-1] + " hundred " + arr1[q-1]
+        }
+        else if(r==1)
+        {
+            return arr1[number-1] + " hundred " + arr2[q-1]
         }
         else 
         return arr1[number-1]+ " hundred " + arr3[r-1] + " " + arr1[q-1]
